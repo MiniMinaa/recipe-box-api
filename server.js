967a19a9e9dacd6ccd9acd6effb952b93a293518
Mina R <minarumang@icloud.com>
2026-03-02 15:49:10 +0100

    add IDs

--- server.js
+++ server.js
@@ -4,8 +4,31 @@ const app = express();
 
 app.use(express.json());
 
-const PORT = 300;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running in https://localhost:${PORT}`);
 });
+
+//sample data
+const recipes = [
+  // adds array with 3 objects in it
+  { id: 1, name: "Swedish pancakes", cuisine: "Swedish", prepTime: "15 mins" },
+  { id: 2, name: "Dolma", cuisine: "Kurdish", prepTime: "60 mins" },
+  { id: 3, name: "Greek Salad", cuisine: "Greek", prepTime: "15 mins" },
+];
+
+app.get("/recipes", (req, res) => {
+  res.json(recepies);
+});
+
+(app.post("/recipes"),
+  (req, res) => {
+    const newRecipe = {
+      id: recipes.length + 1,
+      cuisine: req.body.cuisine,
+      prepTime: recipes.body.prepTime,
+    };
+    books.push(newRecipe);
+    res.json({ message: "Recipe added successfully!", book: newBook });
+  });
