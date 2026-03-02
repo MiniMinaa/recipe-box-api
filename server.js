1eea1fc17404d62da052d04a786dff1744cfbb90
Mina R <minarumang@icloud.com>
2026-03-02 16:23:43 +0100

    fix incorrect HTTPS reference in server startup message

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running in https://localhost:3000/recipe`);
+  console.log(`Server is running in http://localhost:${PORT}`);
 });
 
 //sample data
